ba4c250dcd8f6fe7ed32f572a5b9739a9240176e
yupengtao <yupengtao@88.com>
2023-05-15 17:28:21 +0800

    维护

--- service/service-product/src/main/resources/前端项目/gmall_admin/config/dev.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-// 后端接口地址
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_API: '"http://localhost"'
-})
